439320f5f6dcdc1e4a6a54184dd943b35d9315f1
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-16 13:39:17 -0400

    Tests / GET endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,8 +1,26 @@
 const request = require('supertest');
-const server = require('./server.js');
+const server = require('./server');
 
 describe('server', () => {
   it("sets the environment to 'testing'", () => {
     expect(process.env.DB_ENV).toBe('testing');
+  });
+
+  describe('/ GET', () => {
+    it('should return 200 OK', () => {
+      return request(server)
+        .get('/')
+        .expect(200);
+    });
+    
+    it('should return a JSON object', async () => {
+      const res = await request(server).get('/');
+      expect(res.type).toEqual('application/json');
+    });
+
+    it('should return api:up', async () => {
+      const res = await request(server).get('/');
+      expect(res.body).toEqual({ api: 'up' });
+    });
   });
 });
