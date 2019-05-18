e49d1cb13ee3fd56eb222ff54c091b821425acb7
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-18 16:27:04 -0400

    Catch up w/ Luis' version w/ PG deployment

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -1,5 +1,5 @@
-const db = require('../data/dbConfig');
-const Hobbits = require('./hobbitsModel');
+const db = require('../data/dbConfig.js');
+const Hobbits = require('./hobbitsModel.js');
 
 describe('hobbits model', () => {
   afterEach(async () => {
@@ -7,19 +7,24 @@ describe('hobbits model', () => {
   });
 
   describe('insert()', () => {
-    it('should insert hobbit', async() => {
+    it('should insert provided hobbit', async () => {
       await Hobbits.insert({ name: 'gaffer' });
-      const hobbits = await db('hobbits');
-      expect(hobbits).toHaveLength(1);
-    })
 
-    it('should insert provided hobbit', async() => {
+      const hobbits = await db('hobbits');
+
+      expect(hobbits).toHaveLength(1);
+    });
+
+    it('should insert provided hobbit', async () => {
       let hobbit = await Hobbits.insert({ name: 'gaffer' });
       expect(hobbit.name).toBe('gaffer');
+
       hobbit = await Hobbits.insert({ name: 'sam' });
       expect(hobbit.name).toBe('sam');
+
       const hobbits = await db('hobbits');
+
       expect(hobbits).toHaveLength(2);
-    })
-  })
-})
+    });
+  });
+});
